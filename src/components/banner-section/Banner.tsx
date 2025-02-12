6327b521f940e9c876dc70a2f6dea29dc8593b16
Marcos <marcos.braz@grt8.com.br>
2025-02-12 20:59:19 -0300

    feat: logic of banner

--- src/components/banner-section/Banner.tsx
+++ src/components/banner-section/Banner.tsx
@@ -1,14 +1,55 @@
 import { CaretLeft, CaretRight } from "@phosphor-icons/react";
 import banner from "../../assets/banner.svg";
+import secondBanner from "../../assets/second-banner.svg";
+import { useEffect, useState } from "react";
 
 export default function Banner() {
+  const banners = [banner, secondBanner];
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const [showButtons, setShowButtons] = useState(false);
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentIndex((prevIndex) => (prevIndex + 1) % banners.length);
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) => (prevIndex + 1) % banners.length);
+  };
+
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? banners.length - 1 : prevIndex - 1
+    );
+  };
+
   return (
-    <div className="flex justify-center items-center py-8">
-      <button className="bg-[#F47920] z-10 text-white h-16 w-7 rounded-3xl flex items-center justify-center">
+    <div
+      className="flex justify-center items-center my-8"
+      onMouseEnter={() => setShowButtons(true)}
+      onMouseLeave={() => setShowButtons(false)}
+    >
+      <button
+        onClick={handlePrev}
+        className={`bg-[#F47920] z-10 text-white h-16 w-7 rounded-3xl flex items-center justify-center transition-opacity duration-300 ${
+          showButtons ? "opacity-100" : "opacity-0"
+        }`}
+      >
         <CaretLeft size={19} className="font-bold" />
       </button>
-      <img className="mx-[-15px] z-0" src={banner} />
-      <button className="bg-[#F47920] z-10 text-white h-16 w-7 rounded-3xl flex items-center justify-center">
+      <img
+        className="mx-[-15px] z-0 transition-opacity duration-500"
+        src={banners[currentIndex]}
+      />
+      <button
+        onClick={handleNext}
+        className={`bg-[#F47920] z-10 text-white h-16 w-7 rounded-3xl flex items-center justify-center transition-opacity duration-300 ${
+          showButtons ? "opacity-100" : "opacity-0"
+        }`}
+      >
         <CaretRight size={19} className="font-bold" />
       </button>
     </div>
