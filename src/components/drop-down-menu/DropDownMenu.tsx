6b8d974a6f07aa54a91ed2b79076337860603896
Marcos <marcosaureliobrazdossantos@gmail.com>
2025-02-14 15:42:30 -0300

    feat: creation of dropdown menu

---
+++ src/components/drop-down-menu/DropDownMenu.tsx
@@ -0,0 +1,190 @@
+import caret from "../../assets/Vector.png";
+import elipse from "../../assets/elipse.svg";
+import group from "../../assets/group.svg";
+
+export default function DropDownMenu() {
+  return (
+    <div className="flex flex-col items-center">
+      <div className="flex justify-center mt-5 w-[1196px]">
+        <div className="flex bg-white flex-col gap-10 rounded-lg">
+          <div className="flex gap-20">
+            <div className=" flex flex-col gap-5 w-48">
+              <h1 className="text-[#737373] text-[18px] font-semibold">
+                Saúde e Bem-estar
+              </h1>
+              <div className="flex flex-col gap-5 ml-1 text-[#737373] text-[18px]">
+                <p>Beleza</p>
+                <p>Cuidados Pessoais</p>
+                <p>Esportes e Aventura</p>
+              </div>
+              <div className="flex items-center gap-1">
+                <p className="text-[#737373] text-[14px]">Ver mais</p>
+                <img src={caret} />
+              </div>
+            </div>
+
+            <div className=" flex flex-col gap-5 w-48">
+              <h1 className="text-[#737373] text-[18px] font-semibold">
+                Estilo
+              </h1>
+              <div className="flex flex-col gap-5 ml-1 text-[#737373] text-[18px]">
+                <p>Vestuário</p>
+                <p>Calçados</p>
+                <p>Bijuterias e Acessórios</p>
+              </div>
+              <div className="flex items-center gap-1">
+                <p className="text-[#737373] text-[14px]">Ver mais</p>
+                <img src={caret} />
+              </div>
+            </div>
+
+            <div className=" flex flex-col gap-5 w-48">
+              <h1 className="text-[#737373] text-[18px] font-semibold">
+                Eletro
+              </h1>
+              <div className="flex flex-col gap-5 ml-1 text-[#737373] text-[18px]">
+                <p>Eletrodomésticos</p>
+                <p className="line-clamp-1">Celulares, Tablets e Acessórios</p>
+                <p>Camêras e Acessórios</p>
+              </div>
+              <div className="flex items-center gap-1">
+                <p className="text-[#737373] text-[14px]">Ver mais</p>
+                <img src={caret} />
+              </div>
+            </div>
+          </div>
+
+          <div className="flex gap-20">
+            <div className=" flex flex-col gap-5 w-48">
+              <h1 className="text-[#737373] text-[18px] font-semibold">Casa</h1>
+              <div className="flex flex-col gap-5 ml-1 text-[#737373] text-[18px]">
+                <p>Casa, Jardim e Limpeza</p>
+                <p>Móveis e Decoração</p>
+                <p>Construção</p>
+              </div>
+              <div className="flex items-center gap-1">
+                <p className="text-[#737373] text-[14px]">Ver mais</p>
+                <img src={caret} />
+              </div>
+            </div>
+
+            <div className=" flex flex-col gap-5 w-48">
+              <h1 className="text-[#737373] text-[18px] font-semibold">
+                Tincidunt
+              </h1>
+              <div className="flex flex-col gap-5 ml-1 text-[#737373] text-[18px]">
+                <p>Tempor</p>
+                <p>Consequat</p>
+              </div>
+              <div className="flex items-center gap-1">
+                <p className="text-[#737373] text-[14px]">Ver mais</p>
+                <img src={caret} />
+              </div>
+            </div>
+
+            <div className=" flex flex-col gap-5 w-48">
+              <h1 className="text-[#737373] text-[18px] font-semibold">
+                Ipsum Lorem
+              </h1>
+              <div className="flex flex-col gap-5 ml-1 text-[#737373] text-[18px]">
+                <p>Sit Amet</p>
+                <p>Consequant</p>
+                <p>Pellentesque</p>
+              </div>
+              <div className="flex items-center gap-1">
+                <p className="text-[#737373] text-[14px]">Ver mais</p>
+                <img src={caret} />
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="flex flex-col gap-5 w-48 ml-20">
+          <h1 className="text-[#737373] text-[18px] font-semibold">
+            Mais Departamentos
+          </h1>
+          <div className="flex flex-col gap-5 ml-1 text-[#737373] text-[18px]">
+            <p>Petshop</p>
+            <p>Produtos Regionais</p>
+            <p>Brinquedos e Jogos</p>
+            <p>Livros</p>
+            <p>Música e Hobbies</p>
+            <p>Bebês</p>
+            <p>Alimentos e Bebidas</p>
+            <p>Agro e Indústria</p>
+          </div>
+          <p className="text-[#737373] text-[14px]">
+            Ver todos os departamentos
+          </p>
+        </div>
+      </div>
+      <div className="flex bg-[#faf7f4] p-10 gap-6 rounded-b-lg">
+        <div className="flex flex-col gap-2 items-center">
+          <img src={elipse} />
+          <p className="text-[#737373] text-[18px]">
+            Lorem <br />
+            Ipsum
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 items-center">
+          <img src={elipse} />
+          <p className="text-[#737373] text-[18px]">
+            Lorem <br />
+            Ipsum
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 items-center">
+          <img src={elipse} />
+          <p className="text-[#737373] text-[18px]">
+            Lorem <br />
+            Ipsum
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 items-center">
+          <img src={elipse} />
+          <p className="text-[#737373] text-[18px]">
+            Lorem <br />
+            Ipsum
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 items-center">
+          <img src={elipse} />
+          <p className="text-[#737373] text-[18px]">
+            Lorem <br />
+            Ipsum
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 items-center">
+          <img src={elipse} />
+          <p className="text-[#737373] text-[18px]">
+            Lorem <br />
+            Ipsum
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 items-center">
+          <img src={elipse} />
+          <p className="text-[#737373] text-[18px]">
+            Lorem <br />
+            Ipsum
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 items-center">
+          <img src={elipse} />
+          <p className="text-[#737373] text-[18px]">
+            Lorem <br />
+            Ipsum
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 items-center">
+          <img src={elipse} />
+          <p className="text-[#737373] text-[18px]">
+            Lorem <br />
+            Ipsum
+          </p>
+        </div>
+        <div className="flex flex-col gap-2 items-center">
+          <img src={group} />
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/submenu/SubMenu.tsx
+++ src/components/submenu/SubMenu.tsx
@@ -1,26 +1,36 @@
+import { useState } from "react";
 import e from "../../assets/e.svg";
 
 import { CaretDown, List } from "@phosphor-icons/react";
+import DropDownMenu from "../drop-down-menu/DropDownMenu";
 
 export default function SubMenu() {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="flex justify-between items-center bg-[#F7F3EE] py-4 px-32 gap-7">
-      <div className="flex gap-6">
-        <div className="flex  items-center gap-2">
-          <List color="#F47920" size={20} />
-          <p className="text-base font-semibold text-[#3F3F3F]">
-            Departamentos
-          </p>
-          <CaretDown color="#F47920" size={20} />
+    <div>
+      <div className="flex justify-between items-center bg-[#F7F3EE] py-4 px-32 gap-7">
+        <div className="flex gap-6">
+          <div
+            className="flex items-center gap-2 cursor-pointer"
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <List color="#F47920" size={20} />
+            <p className="text-base font-semibold text-[#3F3F3F]">
+              Departamentos
+            </p>
+            <CaretDown color="#F47920" size={20} />
+          </div>
+          <div className="flex items-center gap-2">
+            <img src={e} alt="e" className="mt-[-4px]" />
+            <p className="text-base font-medium text-[#3F3F3F]">
+              Descontos Cliente Unimed Natal
+            </p>
+          </div>
         </div>
-        <div className="flex items-center gap-2">
-          <img src={e} alt="e" className="mt-[-4px]" />
-          <p className="text-base font-medium text-[#3F3F3F]">
-            Descontos Cliente Unimed Natal
-          </p>
-        </div>
+        <div className="text-[#737373] text-[16px]">Venda no Uniclube</div>
       </div>
-      <div className="text-[#737373] text-[16px]">Venda no Uniclube</div>
+      {isOpen && <DropDownMenu />}
     </div>
   );
 }
